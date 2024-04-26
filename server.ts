9e702daa4517172456df1f99dd370143e17095fd
Rehan <syed.rehanhaque1994@gmail.com>
2024-04-27 00:04:43 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
 
 const startServer = () =>{
-    const prot=process.env.PORT || 3000;
+    const prot=config.port || 3000;
 
    app.listen(prot,() =>{
  console.log(`listening on port:${prot}`  );
