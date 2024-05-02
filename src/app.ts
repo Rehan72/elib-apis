a7bbd0800235dac81050cd0a44d7450c8c79fdc2
Rehan <syed.rehanhaque1994@gmail.com>
2024-05-02 17:29:59 +0530

    create user routers

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,18 @@ import  express, { NextFunction,Request,Response }  from "express";
 import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorhandler";
+import userRouter from "./user/userRouter";
 
 const app = express()
 // Route configuration
 // http methods: "GET", "POST", "PUT", "DELETE"
 app.get('/',(req, res,next) => {
-  const error= createHttpError(400,"something went wrong");
-  throw error
+  // const error= createHttpError(400,"something went wrong");
+  // throw error
   res.json({success:"Welcome to elib apis"})
 })
+
+app.use('/api/users',userRouter)
 
 // Global error handler
 app.use(globalErrorHandler)
